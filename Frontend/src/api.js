930ea7b9b6c0d1f8a69abe66a07ae8a14e1adb64
MeghPrep <cexam666@gmail.com>
2026-07-14 01:17:11 +0530

    Fix API base URL for production environment

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  "http://localhost:3000 || https://megha-quiz-87mn.vercel.app";
+  "http://localhost:3000 || https://megha-quiz.vercel.app";
 
 axios.defaults.baseURL = API_BASE_URL;
 axios.defaults.withCredentials = true;
